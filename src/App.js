64ea86eece357f311636c01f9a2e21fccfba4763
HollyRave <56439013+HollyRave@users.noreply.github.com>
2024-04-01 22:56:42 +0300

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import Header from './components/Header';
+import { createTheme, colors, ThemeProvider } from '@mui/material';
+import Layout from './components/layout/Layout';
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: colors.grey[500]
+    }
+  }
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <div className="App">
+        <Layout />
+      </div>
+    </ThemeProvider >
   );
 }
 
